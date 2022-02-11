be508abd995dfca30b52b920efebd517362f3fb0
Braxton <byasami6@gmail.com>
2022-02-11 12:33:14 -0800

    main

---
+++ PMMC/android/src/com/pmmc/app/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pmmc.app;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pmmc.app.Launcher;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Launcher(), config);
+	}
+}
---
+++ PMMC/desktop/src/com/pmmc/app/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pmmc.app.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pmmc.app.Launcher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Launcher(), config);
+	}
+}
