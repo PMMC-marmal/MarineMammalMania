1ef5c5d6dd2c0395cae9705d09103e532ae062cd
RicCake <kennyton148@gmail.com>
2022-05-06 12:30:02 -0700

    FadeOut

---
+++ PMMC/core/src/com/pmmc/app/transitions/FadeOut.java
@@ -0,0 +1,35 @@
+package com.pmmc.app.transitions;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.pmmc.app.transitions.TransitionEffect;
+
+public class FadeOut extends TransitionEffect {
+
+    Color color = new Color();
+    ShapeRenderer shapeRenderer;
+
+    public FadeOut(float duration) {
+        super(duration);
+        shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setAutoShapeType(true);
+    }
+
+    @Override
+    public void render(Screen current, Screen next) {
+        //current.render(Gdx.graphics.getDeltaTime());
+        color.set(0f, 0f, 0f, getAlpha());
+        Gdx.gl.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        // draw rectangle
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(color);
+        shapeRenderer.rect(0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        shapeRenderer.end();
+        Gdx.gl.glDisable(GL20.GL_BLEND);
+
+    }
+}
